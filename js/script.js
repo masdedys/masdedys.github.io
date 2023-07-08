eb091da0b8b0b5ee3c0a9fad8f1fa876fac6e4cd
masdedys <kakashi.setiawan@gmail.com>
2023-07-08 17:19:44 +0700

    tes baru

--- js/script.js
+++ js/script.js
@@ -3,5 +3,57 @@ Fancybox.bind("[data-fancybox]", {
   // Your custom options
 });
 
-//aos
-AOS.init();
+function TampilkanSemuaMenu() {
+  $.getJSON("listjualan.json", function (data) {
+    //console.log(data);
+    let menu = data.menu;
+    //console.log(menu);
+    $.each(menu, function (i, data) {
+      // console.log(data);
+      $("#daftar-menu").append(
+        '<div class="card w-96 bg-base-100 shadow-xl py-5" data-aos="flip-left"><a href="assets/portofolio/' +
+          data.gambar +
+          '" data-fancybox="gallery" data-caption="Photoshop - Digital Imaging"><figure><img src="assets/portofolio/' +
+          data.gambar +
+          '" width="70%" height="70%" class="rounded-xl" alt="Shoes" /></figure></a><div class="card-body"><h4 class="card-title items-center text-center">' +
+          data.nama +
+          '</h4><div class="card-actions justify-end"><button class="btn btn-primary" onclick="my_modal_1.showModal()">Buy Now</button><dialog id="my_modal_1" class="modal"><form method="dialog" class="modal-box"><h3 class="font-bold text-lg">Hello!</h3><p class="py-4">Mohon maaf atas ketidaknyamanannya, pemesanan masih dilakukan secara manual. Kamu bisa DM saya di Instagram saya di mas_dedys. Atau klik tombol ini, <br /><br /><button class="btn btn-primary"><a href="https://www.instagram.com/mas_dedys/" target="_blank"><i class="bi bi-instagram" style="font-size: 25px"></i></a></button></p><div class="modal-action"><!-- if there is a button in form, it will close the modal --><button class="btn">Close</button></div></form></dialog></div></div></div>'
+      );
+    });
+  });
+}
+
+TampilkanSemuaMenu();
+
+$(".link").on("click", function () {
+  $(".link").removeClass("active");
+  $(this).addClass("active");
+
+  let category = $(this).html();
+  //   console.log(category);
+  $("h1").html(category);
+
+  if (category == "All Product") {
+    TampilkanSemuaMenu();
+    return;
+  }
+
+  $.getJSON("listjualan.json", function (data) {
+    let menu = data.menu;
+    let content = "";
+
+    $.each(menu, function (i, data) {
+      if (data.kategori == category.toLowerCase()) {
+        content +=
+          '<div class="card w-96 bg-base-100 shadow-xl py-5" data-aos="flip-left"><a href="assets/portofolio/' +
+          data.gambar +
+          '" data-fancybox="gallery" data-caption="Photoshop - Digital Imaging"><figure><img src="assets/portofolio/' +
+          data.gambar +
+          '" width="70%" height="70%" class="rounded-xl" alt="Shoes" /></figure></a><div class="card-body"><h4 class="card-title items-center text-center">' +
+          data.nama +
+          '</h4><div class="card-actions justify-end"><button class="btn btn-primary" onclick="my_modal_1.showModal()">Buy Now</button><dialog id="my_modal_1" class="modal"><form method="dialog" class="modal-box"><h3 class="font-bold text-lg">Hello!</h3><p class="py-4">Mohon maaf atas ketidaknyamanannya, pemesanan masih dilakukan secara manual. Kamu bisa DM saya di Instagram saya di mas_dedys. Atau klik tombol ini, <br /><br /><button class="btn btn-primary"><a href="https://www.instagram.com/mas_dedys/" target="_blank"><i class="bi bi-instagram" style="font-size: 25px"></i></a></button></p><div class="modal-action"><!-- if there is a button in form, it will close the modal --><button class="btn">Close</button></div></form></dialog></div></div></div>';
+      }
+    });
+    $("#daftar-menu").html(content);
+  });
+});
