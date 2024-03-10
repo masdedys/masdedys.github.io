be078a2fd75664bf2b3bb0a97dabdb78f208aa87
masdedys <kakashi.setiawan@gmail.com>
2024-03-11 06:04:53 +0700

    pindah style masdedyshop ke style.css

--- js/script.js
+++ js/script.js
@@ -4,32 +4,11 @@ Fancybox.bind("[data-fancybox]", {
 });
 
 function TampilkanSemuaMenu() {
-  // $.getJSON("js/listjualan.json", function (data) {
-  //   //console.log(data);
-  //   let menu = data.menu;
-  //   //console.log(menu);
-  //   $.each(menu, function (i, data) {
-  //     // console.log(data);
-  //     $("#daftar-menu").append(
-  //       '<div class="card col-sm-3 m-3"><img src="assets/jualan/' +
-  //         data.gambar +
-  //         '" width="18rem" class="card-img-top" alt="' +
-  //         data.gambar +
-  //         '"><div class="card-body"><h5 class="card-title">' +
-  //         data.nama +
-  //         "</h5><p>Rp." +
-  //         data.harga +
-  //         '</p><p style="font-size: 16px;"></p><a href="detail-jualan/' +
-  //         data.detail +
-  //         '" class="btn">Detail</a></div></div>'
-  //     );
-  //   });
-  // });
-
   $.getJSON("js/listjualan.json", function (data) {
     //console.log(data);
     let menu = data.menu;
     //console.log(menu);
+    $("#daftar-menu").empty();
     $.each(menu, function (i, data) {
       // console.log(data);
       $("#daftar-menu").append(
@@ -65,21 +44,6 @@ $(".menu-info").on("click", function () {
   $.getJSON("js/listjualan.json", function (data) {
     let menu = data.menu;
     let content = "";
-
-    // $.each(menu, function (i, data) {
-    //   if (data.kategori == category.toLowerCase()) {
-    //     content +=
-    //       '<div class="card col-sm-3 m-3"><img src="assets/jualan/' +
-    //       data.gambar +
-    //       '" width="18rem" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">' +
-    //       data.nama +
-    //       "</h5><p>Rp." +
-    //       data.harga +
-    //       '</p><p style="font-size: 16px;"></p><a href="detail-jualan/' +
-    //       data.detail +
-    //       '" class="btn">Detail</a></div></div>';
-    //   }
-    // });
 
     $.each(menu, function (i, data) {
       if (data.kategori == category.toLowerCase()) {
